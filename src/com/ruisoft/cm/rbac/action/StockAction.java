f01bf0f918ba85b20b39278a52cd845d1f480b31
flsah <flsah.cn@gmail.com>
2014-01-21 22:20:31 +0800

    提交工程配置文件

---
+++ src/com/ruisoft/cm/rbac/action/StockAction.java
@@ -0,0 +1,10 @@
+package com.ruisoft.cm.rbac.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stock.do")
+public class StockAction extends CommonAction {
+	
+}
